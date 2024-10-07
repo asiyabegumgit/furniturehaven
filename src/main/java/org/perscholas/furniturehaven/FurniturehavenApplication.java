d28e60c1ea52aad1d4a948a21ecb129c93759b7e
Asiya Begum Sheik Mohamed <asiyabegum.sheikmohamed@gmail.com>
2024-10-07 11:45:39 -0400

    Initial commit

---
+++ src/main/java/org/perscholas/furniturehaven/FurniturehavenApplication.java
@@ -0,0 +1,13 @@
+package org.perscholas.furniturehaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurniturehavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurniturehavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/perscholas/furniturehaven/FurniturehavenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.perscholas.furniturehaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurniturehavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
